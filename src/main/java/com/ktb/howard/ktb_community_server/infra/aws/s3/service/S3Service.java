70f7e8b60ba5c1726e19a0d0b4444f3bd044e64d
Howard <62272939+juyoungIt@users.noreply.github.com>
2025-12-09 00:21:33 +0900

    refactor[#159]: S3 Presigned URL 관련 Redis Caching 적용

--- src/main/java/com/ktb/howard/ktb_community_server/infra/aws/s3/service/S3Service.java
+++ src/main/java/com/ktb/howard/ktb_community_server/infra/aws/s3/service/S3Service.java
@@ -6,6 +6,7 @@ import com.ktb.howard.ktb_community_server.infra.aws.s3.exception.FileStorageExc
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import software.amazon.awssdk.core.exception.SdkException;
@@ -61,6 +62,7 @@ public class S3Service {
         }
     }
 
+    @Cacheable(value = "s3:get-presigned-url", key = "#objectKey", unless = "#result == null")
     public PresignedUrl createGetObjectPresignedUrl(String objectKey) {
         try {
             GetObjectRequest objectRequest = GetObjectRequest.builder()
